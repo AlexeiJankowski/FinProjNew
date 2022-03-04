817a7e0d0ae42c11d7a97fc0436c4d3bfbb7f47a
Alexey Jankovski <alexey.cde@gmail.com>
2022-03-04 20:26:20 +0100

    index commit

--- FinProjNew/Pages/Index.cshtml.cs
+++ FinProjNew/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinProjNew.Core;
+using FinProjNew.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,15 +13,43 @@ namespace FinProjNew.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ApplicationContext context)
         {
             _logger = logger;
+            _context = context;
         }
+
+        [BindProperty]
+        public QuoteParam Param { get; set; }
+        [BindProperty]
+        public List<Ticker> TickersList { get; set; }
+        [BindProperty]
+        public List<Quote> QuotesList { get; set; }
 
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnPost()
+        {
+            TickersList = _context.Tickers.ToList();
+            QuotesList = CreateRequest(Param);
+
+            return Page();
+        }
+
+        public List<Quote> CreateRequest(QuoteParam param)
+        {
+            var url = $"https://finance.yahoo.com/quote/{param.Ticker}/history?period1={(param.StartPeriod.AddDays(1)).ToUnixTimeSeconds()}&period2={param.EndPeriod.ToUnixTimeSeconds()}&interval={param.Period}&filter=history&frequency=1d&includeAdjustedClose=true";
+
+            var quotes = new List<double>();
+            var datesHeaders = Parse.GetQuotesHeaders(ref quotes, url);
+            List<Quote> Quotes = Parse.ParseQuotes(datesHeaders, quotes);
+
+            return Quotes;
         }
     }
 }
